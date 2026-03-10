c67750ccaee233567beca8d2a22294fb0805bb35
BaeYoungWhan <uee8351773@naver.com>
2026-03-10 19:26:03 +0900

    260310 레파지토리 구성 완료.

---
+++ AIpassBackend/src/main/java/com/aipass/AipassApplication.java
@@ -0,0 +1,13 @@
+package com.aipass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AipassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AipassApplication.class, args);
+	}
+
+}
---
+++ AIpassBackend/src/test/java/com/aipass/AipassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aipass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AipassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
